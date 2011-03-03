2bf56b8f1c977df87c5a952c4fe3206993ea1750
Jason Moore <moorepants@gmail.com>
2011-03-03 11:58:07 -0800

    made the output of get_run a little cleaner with regular python types

--- DataProcessor.py
+++ DataProcessor.py
@@ -1,6 +1,26 @@
 import tables as tab
 import numpy as np
 import os
+import re
+
+def create_database():
+    '''Creates an HDF5 file for data collected from the instrumented bicycle'''
+
+    # load the latest file in the data/h5 directory
+    pathtoh5 = os.path.join('..', 'BicycleDAQ', 'data', 'h5')
+    files = os.listdir(pathtoh5)
+    files.sort()
+    rundata = get_run_data(os.path.join(pathtoh5, files[-1]))
+    # set up the table description
+    class Run(tab.IsDescription):
+        # add all of the column headings from par, NICols and VNavCols
+        a = 1.
+
+def parse_vnav_string(vnstr):
+    '''Gets the good info from a VNav string'''
+    vnstr = re.sub('$(.*)\*', '\1', vnstr)
+    vnlist = vnstr.split(',')
+    return vnlist
 
 def get_run_data(pathtofile):
     '''Returns data from the run h5 files using pytables and formats it better
@@ -29,10 +49,10 @@ def get_run_data(pathtofile):
     rundata['VNavData'] = runfile.root.VNavData.read()
 
     # now create two lists that give the column headings for the two data sets
-    rundata['VNavCols'] = list(runfile.root.VNavCols.read())
+    rundata['VNavCols'] = [str(x) for x in runfile.root.VNavCols.read()]
     rundata['NICols'] = []
     for col in runfile.root.InputPairs:
-        rundata['NICols'].append((col.name, int(col.read()[0])))
+        rundata['NICols'].append((str(col.name), int(col.read()[0])))
 
     rundata['NICols'].sort(key=lambda x: x[1])
 
@@ -41,10 +61,17 @@ def get_run_data(pathtofile):
     # put the parameters into a dictionary
     rundata['par'] = {}
     for col in runfile.root.par:
-        rundata['par'][col.name] = col.read()[0]
+        # convert them to regular python types
+        try:
+            if col.name == 'Speed':
+                rundata['par'][col.name] = float(col.read()[0])
+            else:
+                rundata['par'][col.name] = int(col.read()[0])
+        except:
+            rundata['par'][col.name] = str(col.read()[0])
 
     # get the VNavDataText
-    rundata['VNavDataText'] = list(runfile.root.VNavDataText.read())
+    rundata['VNavDataText'] = [str(x) for x in runfile.root.VNavDataText.read()]
 
     # close the file
     runfile.close()
