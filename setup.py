7ad4fcb6f09294c002a3b03842c255a439770061
Jason Moore <moorepants@gmail.com>
2011-07-08 16:59:06 -0700

    adding some files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='DataProcessor',
+    version='0.1.0dev',
+    author='Jason Keith Moore',
+    author_email='moorepants@gmail.com',
+    packages=['dataprocessor',
+              'database',
+              'signalprocessing',
+              'dataprocessor.test'],
+    url='http://github.com/moorepants/InstrumentedBicycle/DataProcessor',
+    license='LICENSE.txt',
+    description='Processes the data collected from the instrumented bicycle.'
+    long_description=open('README.rst').read(),
+)
