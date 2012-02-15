3fa95871a664aee7c1c40175ca243f3fcc3a524f
Jason Moore <moorepants@gmail.com>
2012-02-15 14:13:06 -0800

    Change to setuptools so the development install could be used.

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
       name='BicycleDataProcessor',
       version='0.1.0dev',
       author='Jason Keith Moore',
       author_email='moorepants@gmail.com',
-      packages=['bicycledataprocessor',
-                'bicycledataprocessor.test'],
+      packages=find_packages(),
       url='http://github.com/moorepants/BicycleDataProcessor',
       license='LICENSE.txt',
       description='''Processes the data collected from the instrumented
