060d5a9be644f6c22eb5ee8f4f8a3b957a4f42d0
StefenYin <zqyin@ucdavis.edu>
2012-10-31 09:26:15 -0700

    Built select_runs func

--- bicycledataprocessor/database.py
+++ bicycledataprocessor/database.py
@@ -361,6 +361,45 @@ class DataSet(object):
             pass
         self.close()
 
+    def select_runs(self, riders, maneuvers, environments):
+        """Returns a list of runs given a set of conditions.
+
+        Parameters
+        ----------
+        riders : list
+        All or a subset of ['Charlie', 'Jason', 'Luke'].
+        maneuvers : list
+        All or a subset of ['Balance', 'Balance With Disturbance', 
+                'Track Straight Line', 'Track Straight Line With Disturbance'].
+        environments : list
+        All or a subset of ['Horse Treadmill', 'Pavillion Floor'].
+
+        Returns
+        -------
+        runs : list
+        List of run numbers for the given conditions.
+
+        """
+
+        self.open()
+
+        table = self.database.root.runTable
+
+        runs = []
+        for row in table.iterrows():
+            con = []
+            con.append(row['Rider'] in riders)
+            con.append(row['Maneuver'] in maneuvers)
+            con.append(row['Environment'] in environments)
+            con.append(row['corrupt'] is not True)
+            con.append(int(row['RunID']) > 100)
+            if False not in con:
+                runs.append(row['RunID'])
+
+        self.close()
+
+        return runs
+
     def sync_data(self, directory='exports/'):
         """Synchronizes data to the biosport website."""
         user = 'biosport'
